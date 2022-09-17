d0ff2086ad7e2505c6b2feb6d9e104699a7d3a1c
Aayushd18 <deshmukhaayush81@gmail.com>
2022-09-17 18:45:27 +0530

    Add styles to navbar

--- my-app/components/nav.js
+++ my-app/components/nav.js
@@ -1,6 +1,7 @@
 import styles from "../styles/Nav.module.css";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import Link from "next/link";
+import Image from "next/image"
 
 
 export default function Nav() {
@@ -9,22 +10,41 @@ export default function Nav() {
     return (
       <section className={styles.navContainer}>
         <Link href="/">
-        <h1 className={styles.title}>LW3 DAO</h1>
+        <div className="flex items-center pt-4">
+          <Image src="/lw3-logo-white.png" height={50} width={250}></Image>
+          <h1 className=" text-3xl pb-2 pt-1 ml-2 font-semibold first-letter:text-pink-500">DAO</h1>
+        </div>
       </Link>
       {address ? (
-        <ul className={styles.navItems}>
-          <Link href="/Profile">
-            <li>CREATE PROPOSAL</li>
+        <ul className="flex flex-row justify-between items-center pt-4">
+          <Link href="/">
+            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}>Home</li>
+          </Link>
+          <Link href="https://learnweb3.io/">
+            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}>Main Site</li>
           </Link>
           <Link href="/Profile">
-            <li>PREVIOUS PROPOSALS</li>
+            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-6`}>PROPOSALs</li>
           </Link>
           <li>
         <ConnectWallet accentColor="#162C68" colorMode="dark" />
           </li>
         </ul>
       ) : (
-        <ConnectWallet accentColor="#162C68" colorMode="dark" />
+        <ul className="flex flex-row justify-between items-center pt-4">
+          <Link href="/">
+            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}>Home</li>
+          </Link>
+          <Link href="https://learnweb3.io/">
+            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}>Main Site</li>
+          </Link>
+          <Link href="/team">
+            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-6`}>Team</li>
+          </Link>
+          <li>
+            <ConnectWallet accentColor="#162C68" colorMode="dark" />
+          </li>
+        </ul>
       )}
       </section>
     )
