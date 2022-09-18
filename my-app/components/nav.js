99b0451171cb8fd85d0dba3660bfade60e302a18
Jossif Elefteriadis <jossif_@hotmail.com>
2022-09-18 13:13:10 +0200

    Add <a> tag for LW3 link to open in new tab

--- my-app/components/nav.js
+++ my-app/components/nav.js
@@ -1,54 +1,85 @@
 import styles from "../styles/Nav.module.css";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import Link from "next/link";
-import Image from "next/image"
-
+import Image from "next/image";
 
 export default function Nav() {
   const address = useAddress();
 
-    return (
-      <section className={styles.navContainer}>
-        <Link href="/">
+  return (
+    <section className={styles.navContainer}>
+      <Link href="/">
         <div className="flex items-center pt-4 cursor-pointer">
           <Image src="/lw3-logo-white.png" height={50} width={250}></Image>
-          <h1 className=" text-3xl pb-2 pt-1 font-semibold first-letter:text-pink-500">DAO</h1>
+          <h1 className=" text-3xl pb-2 pt-1 font-semibold first-letter:text-pink-500">
+            DAO
+          </h1>
         </div>
       </Link>
       {address ? (
         <ul className="flex flex-row justify-between items-center pt-4">
           <Link href="/">
-            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}>Home</li>
+            <li
+              className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}
+            >
+              Home
+            </li>
           </Link>
           <Link href="https://learnweb3.io/">
-            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}>Main Site</li>
+            <a target="_blank">
+              <li
+                className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}
+              >
+                Main Site
+              </li>
+            </a>
           </Link>
           <Link href="/proposals">
-            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-6`}>PROPOSALs</li>
+            <li
+              className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-6`}
+            >
+              PROPOSALs
+            </li>
           </Link>
           <Link href="/team">
-            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-6`}>Team</li>
+            <li
+              className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-6`}
+            >
+              Team
+            </li>
           </Link>
           <li>
-        <ConnectWallet accentColor="#162C68" colorMode="dark" />
+            <ConnectWallet accentColor="#162C68" colorMode="dark" />
           </li>
         </ul>
       ) : (
         <ul className="flex flex-row justify-between items-center pt-4">
           <Link href="/">
-            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}>Home</li>
+            <li
+              className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}
+            >
+              Home
+            </li>
           </Link>
           <Link href="https://learnweb3.io/">
-            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}>Main Site</li>
+            <li
+              className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-4`}
+            >
+              Main Site
+            </li>
           </Link>
           <Link href="/team">
-            <li className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-6`}>Team</li>
+            <li
+              className={`${styles.link} ${styles.link_page} cursor-pointer text-lg mr-6`}
+            >
+              Team
+            </li>
           </Link>
           <li>
             <ConnectWallet accentColor="#162C68" colorMode="dark" />
           </li>
         </ul>
       )}
-      </section>
-    )
-  }
+    </section>
+  );
+}
